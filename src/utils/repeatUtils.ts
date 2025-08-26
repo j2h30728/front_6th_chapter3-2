a14866da5029e7a8c7d144e0e1856a1a450a88a1
jihyeon <rachel2148072@gmail.com>
2025-08-26 21:25:03 +0900

    feat: 🍏--repeat.type이 daily이면 시작일부터 종료일까지 매일 날짜를 생성한다.

--- src/utils/repeatUtils.ts
+++ src/utils/repeatUtils.ts
@@ -1,8 +1,29 @@
 import { Event } from '../types';
+import { formatDate } from './dateUtils';
+
+const ONE_DAY = 60 * 60 * 24 * 1000;
 
 export function generateRecurringDates(event: Event) {
-  if (event.repeat.type === 'none') {
-    return [event.date];
+  const repeatType = event.repeat.type;
+  const startData = event.date;
+
+  if (repeatType === 'none') {
+    return [startData];
   }
+
+  if (repeatType === 'daily') {
+    const endDate = event.repeat.endDate || '2025-10-30';
+    const endDateTime = new Date(endDate).getTime();
+    const startDateTime = new Date(event.date).getTime();
+
+    const dateArr = [];
+    let temp = startDateTime;
+    while (temp <= endDateTime) {
+      dateArr.push(formatDate(new Date(temp)));
+      temp += ONE_DAY;
+    }
+    return dateArr;
+  }
+
   return event;
 }
