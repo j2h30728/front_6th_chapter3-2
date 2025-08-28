1436082a359271ad02b5c0816c94edf365a21b31
jihyeon <rachel2148072@gmail.com>
2025-08-29 06:34:12 +0900

    chore: playwright 세팅 변경

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './src/__tests__/e2e',
   timeout: 30 * 1000,
   expect: {
     timeout: 5000,
